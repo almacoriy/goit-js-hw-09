0e87580a664670c7798e66bc3155f788abe42156
almacoriy <amazon407@gmail.com>
2021-12-24 13:35:13 +0200

    add task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+refs.startBtn.addEventListener('click', onStart);
+refs.stopBtn.addEventListener('click', onStop);
+
+function onStart() {
+  intervalId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+    refs.stopBtn.removeAttribute('disabled');
+    refs.startBtn.setAttribute('disabled', true);
+  }, 1000);
+}
+
+function onStop() {
+  clearInterval(intervalId);
+
+  refs.stopBtn.setAttribute('disabled', true);
+  refs.startBtn.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
