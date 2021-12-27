fcb273a7514aaaf562fc9f6464dc0f5f4fd8dda1
almacoriy <amazon407@gmail.com>
2021-12-27 02:33:25 +0200

    add task 02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,88 +1,62 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
 require('flatpickr/dist/themes/dark.css');
 
-// const fp = flatpickr('#datetime-picker', {
-//   enableTime: true,
-//   time_24hr: true,
-//   defaultDate: new Date(),
-//   minuteIncrement: 1,
-//   onClose(selectedDates) {
-//     const currentDate = new Date();
-//     const timeDiff = selectedDates[0] - currentDate;
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  dataDays: document.querySelector('span[data-days]'),
+  dataHours: document.querySelector('span[data-hours]'),
+  dataMin: document.querySelector('span[data-minutes]'),
+  dataSec: document.querySelector('span[data-seconds]'),
+  field: document.querySelectorAll('.field'),
+  input: document.querySelector('#datetime-picker'),
+  timer: document.querySelector('.timer'),
+};
 
-//     if (timeDiff <= 0) {
-//       alert('Please choose a date in the future');
-//     }
-//     refs.startBtn.removeAttribute('disabled');
-//     // onStartTimer();
-//   },
-// });
+refs.startBtn.setAttribute('disabled', true);
+refs.startBtn.addEventListener('click', onStartTimer);
 
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  onClose(selectedDates) {
-    onGetCurrentDate(selectedDates);
-    console.log(selectedDates);
+  onClose() {
+    onGetCurrentDate();
   },
 };
 
 const fp = flatpickr('#datetime-picker', { ...options });
-let timeDiff = null;
 
-// console.log(fp.config.defaultDate);
-
-const refs = {
-  input: document.querySelector('#datetime-picker'),
-  startBtn: document.querySelector('button[data-start]'),
-  timer: document.querySelector('.timer'),
-  field: document.querySelectorAll('.field'),
-  dataDays: document.querySelector('value[data-days]'),
-  dataHours: document.querySelector('value[data-hours]'),
-  dataMin: document.querySelector('value[data-minutes]'),
-  dataSec: document.querySelector('value[data-seconds]'),
-};
-
-// console.log(fp.selectedDates[0]);
-
-//  Немного стилей
-refs.input.style.width = '220px';
-refs.input.style.height = '40px';
-refs.input.style.fontSize = '22px';
-refs.startBtn.style.width = '80px';
-refs.startBtn.style.height = '40px';
-refs.startBtn.style.fontSize = '22px';
-refs.timer.style.display = 'flex';
-refs.field.forEach(elem => {
-  elem.style.display = 'flex';
-  elem.style.flexDirection = 'column';
-  elem.style.alignItems = 'center';
-  elem.style.padding = '10px';
-  elem.style.fontSize = '20px';
-});
-
-refs.startBtn.setAttribute('disabled', true);
-
-//  Проверяем разницу выбранной и текукщей даты
-
-function onGetCurrentDate(date) {
-  const currentDate = new Date();
-  timeDiff = date[0] - currentDate;
-
-  // console.log(currentDate);
-  // console.log(selectedDates[0]);
+function onGetCurrentDate() {
+  if (fp.selectedDates[0] < fp.config.defaultDate.getTime()) {
+    Notiflix.Notify.failure('Please choose a date in the future');
+    return;
+  }
+  refs.startBtn.removeAttribute('disabled');
 }
-
-//?  Доделать  ===================================================
 
 function onStartTimer() {
-  refs.startBtn.addEventListener('click', onClickBtn);
+  const intervalId = setInterval(() => {
+    let timeDiff = fp.selectedDates[0] - new Date();
+    let time = convertMs(timeDiff);
+
+    refs.dataDays.textContent = addLeadingZero(time.days);
+    refs.dataHours.textContent = addLeadingZero(time.hours);
+    refs.dataMin.textContent = addLeadingZero(time.minutes);
+    refs.dataSec.textContent = addLeadingZero(time.seconds);
+
+    if (timeDiff <= 1000) {
+      clearInterval(intervalId);
+    }
+  }, 1000);
 }
 
-function addLeadingZero(value) {}
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -102,3 +76,20 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
+
+//=======Немного стилей=======
+
+refs.input.style.width = '220px';
+refs.input.style.height = '40px';
+refs.input.style.fontSize = '22px';
+refs.startBtn.style.width = '80px';
+refs.startBtn.style.height = '40px';
+refs.startBtn.style.fontSize = '22px';
+refs.timer.style.display = 'flex';
+refs.field.forEach(elem => {
+  elem.style.display = 'flex';
+  elem.style.flexDirection = 'column';
+  elem.style.alignItems = 'center';
+  elem.style.padding = '10px';
+  elem.style.fontSize = '20px';
+});
